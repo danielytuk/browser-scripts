e0ad812eed75b2effed8acad2d8baf3104c0f760
Daniel Back <daniel@dytuk.media>
2024-03-16 20:11:54 +0000

    Create index.js

---
+++ piped-video-enhancer/index.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Piped Video Enhancer
+// @description  Enhances the Piped video player interface by hiding the navbar and extending the video display area
+// @author       danielytuk
+// @version      1.0
+// @grant        none
+// @match        https://piped.smnz.de/watch?v=*
+// @run-at       document-idle
+// @icon         https://cdn.statically.io/gh/TeamPiped/Piped/3a78b19a/public/favicon.ico
+// ==/UserScript==
+
+(() => {
+    'use strict';
+
+    let handleScroll = null;
+
+    const applyCinemaMode = (navbar, videoPlayer) => {
+        const toggleNavbar = () => navbar.style.display = window.scrollY === 0 ? "none" : "block";
+        Object.assign(videoPlayer.style, { width: "100%", height: "calc(100vh - 50px)", maxHeight: "131vh" });
+        toggleNavbar();
+        window.addEventListener('scroll', handleScroll);
+    };
+
+    const checkForElements = () => {
+        const [navbar, videoPlayer] = [document.querySelector('nav'), document.querySelector('.player-container')];
+        if (navbar && videoPlayer) {
+            handleScroll = () => navbar.style.display = window.scrollY === 0 ? "none" : "block";
+            applyCinemaMode(navbar, videoPlayer);
+        } else setTimeout(checkForElements, 1000);
+    };
+
+    window.addEventListener('beforeunload', (event) => {
+        if (handleScroll) {
+            window.removeEventListener('scroll', handleScroll);
+            event.preventDefault(); // Prevent the default behavior (page reload)
+        }
+    });
+
+    checkForElements();
+})();
