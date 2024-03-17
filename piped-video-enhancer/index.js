f6ea595669a7a90dbace9a4639f7c871aa131fee
Daniel Back <daniel@dytuk.media>
2024-03-17 02:50:38 +0000

    Update index.js

--- piped-video-enhancer/index.js
+++ piped-video-enhancer/index.js
@@ -1,26 +1,17 @@
+// ==UserScript==
 // @name         Piped Video Enhancer
 // @description  Enhances the Piped video player interface by hiding the navbar and extending the video display area
 // @author       danielytuk
+// @match        https://pipedapi.kavin.rocks/watch?v=*
+// @match        https://pipedapi.syncpundit.io/watch?v=*
+// @match        https://piped-api.lunar.icu/watch?v=*
 // @version      1.5
 // @grant        none
 // @run-at       document-idle
 // @icon         https://cdn.statically.io/gh/TeamPiped/Piped/3a78b19a/public/favicon.ico
-// @match        https://pipedapi.kavin.rocks/watch?v=*
-// @match        https://pipedapi.syncpundit.io/watch?v=*
-// @match        https://piped-api.lunar.icu/watch?v=*
 // @match        https://pipedapi.r4fo.com/watch?v=*
 // @match        https://pipedapi.darkness.services/watch?v=*
-// @match        https://pipedapi-libre.kavin.rocks/watch?v=*
-// @match        https://api.piped.projectsegfau.lt/watch?v=*
-// @match        https://pipedapi.us.projectsegfau.lt/watch?v=*
-// @match        https://api.piped.privacydev.net/watch?v=*
-// @match        https://pipedapi.smnz.de/watch?v=*
-// @match        https://pipedapi.adminforge.de/watch?v=*
-// @match        https://pipedapi.astartes.nl/watch?v=*
-// @match        https://pipedapi.drgns.space/watch?v=*
-// @match        https://pipedapi.ducks.party/watch?v=*
-// @match        https://pipedapi.ngn.tf/watch?v=*
-// @match        https://pipedapi.coldforge.xyz/watch?v=*
+// ==/UserScript==
 
 (() => {
     'use strict';
