0e0db6feb6c2b53d157ffe5cfef91d0abf639528
GitHub Action <action@github.com>
2024-03-17 02:42:46 +0000

    Update piped-video-enhancer domains

--- piped-video-enhancer/index.js
+++ piped-video-enhancer/index.js
@@ -1,8 +1,7 @@
-// ==UserScript==
 // @name         Piped Video Enhancer
 // @description  Enhances the Piped video player interface by hiding the navbar and extending the video display area
 // @author       danielytuk
-// @version      1.1
+// @version      1.2
 // @grant        none
 // @match        https://pipedapi.kavin.rocks/watch?v=*
 // @match        https://pipedapi.syncpundit.io/watch?v=*
@@ -20,29 +19,4 @@
 // @match        https://pipedapi.ducks.party/watch?v=*
 // @match        https://pipedapi.ngn.tf/watch?v=*
 // @match        https://pipedapi.coldforge.xyz/watch?v=*
-    let handleScroll = null;
-
-    const applyCinemaMode = (navbar, videoPlayer) => {
-        const toggleNavbar = () => navbar.style.display = window.scrollY === 0 ? "none" : "block";
-        Object.assign(videoPlayer.style, { width: "100%", height: "calc(100vh - 50px)", maxHeight: "131vh" });
-        toggleNavbar();
-        window.addEventListener('scroll', handleScroll);
-    };
-
-    const checkForElements = () => {
-        const [navbar, videoPlayer] = [document.querySelector('nav'), document.querySelector('.player-container')];
-        if (navbar && videoPlayer) {
-            handleScroll = () => navbar.style.display = window.scrollY === 0 ? "none" : "block";
-            applyCinemaMode(navbar, videoPlayer);
-        } else setTimeout(checkForElements, 1000);
-    };
-
-    window.addEventListener('beforeunload', (event) => {
-        if (handleScroll) {
-            window.removeEventListener('scroll', handleScroll);
-            event.preventDefault(); // Prevent the default behavior (page reload)
-        }
-    });
-
-    checkForElements();
-})();
+// ==/UserScript==
