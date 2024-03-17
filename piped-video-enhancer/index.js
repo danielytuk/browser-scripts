562ea2c4e2504d51a5fd56090041508009cf3737
GitHub Action <action@github.com>
2024-03-17 22:30:15 +0000

    Update piped-video-enhancer domains

--- piped-video-enhancer/index.js
+++ piped-video-enhancer/index.js
@@ -2,29 +2,17 @@
 // @name         Piped Video Enhancer
 // @description  Enhances the Piped video player interface by hiding the navbar, extending the video display area, and setting video resolution to 1080p automatically
 // @icon         https://cdn.statically.io/gh/TeamPiped/Piped/3a78b19a/public/favicon.ico
-// @match        https://piped.lunar.icu//watch?v=*
-// @match        https://piped.projectsegfau.lt//watch?v=*
-// @match        https://piped.privacydev.net//watch?v=*
-// @match        https://piped.smnz.de//watch?v=*
-// @match        https://piped.adminforge.de//watch?v=*
-// @match        https://piped.astartes.nl//watch?v=*
-// @match        https://piped.drgns.space//watch?v=*
-// @match        https://piped.ngn.tf//watch?v=*
-// @match        https://piped.us.projectsegfau.lt//watch?v=*
-// @match        https://piped.ducks.party//watch?v=*
+// @match        https://piped.lunar.icu/watch?v=*
+// @match        https://piped.projectsegfau.lt/watch?v=*
+// @match        https://piped.privacydev.net/watch?v=*
+// @match        https://piped.smnz.de/watch?v=*
+// @match        https://piped.adminforge.de/watch?v=*
+// @match        https://piped.astartes.nl/watch?v=*
+// @match        https://piped.drgns.space/watch?v=*
+// @match        https://piped.ngn.tf/watch?v=*
+// @match        https://piped.ducks.party/watch?v=*
 // @grant        none
 // @author       danielytuk
-// @version      1.19
+// @version      1.20
 // @run-at       document-idle
 // ==/UserScript==
-    window.addEventListener('scroll', scrollHandler);
-    window.addEventListener('beforeunload', (event) => {
-        if (handleScroll) {
-            handleScroll = null;
-            window.removeEventListener('scroll', scrollHandler);
-            observer.disconnect();
-            event.preventDefault();
-    });
-    window.addEventListener('DOMContentLoaded', checkForElements);
-})();
-checkForElements);
