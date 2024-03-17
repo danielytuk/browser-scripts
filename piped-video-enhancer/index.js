d342b81cc69ceeca38b9999cfcc7bada78335302
GitHub Action <action@github.com>
2024-03-17 22:33:58 +0000

    Update piped-video-enhancer domains

--- piped-video-enhancer/index.js
+++ piped-video-enhancer/index.js
@@ -2,11 +2,20 @@
 // @name         Piped Video Enhancer
 // @description  Enhances the Piped video player interface by hiding the navbar, extending the video display area, and setting video resolution to 1080p automatically
 // @icon         https://cdn.statically.io/gh/TeamPiped/Piped/3a78b19a/public/favicon.ico
-// @match        https://piped.lunar.icu/watch?v=*
 // @grant        none
 // @author       danielytuk
-// @version      1.20
+// @version      1.21
 // @run-at       document-idle
+// @match        https://piped.lunar.icu/watch?v=*
+// @match        https://piped.projectsegfau.lt/watch?v=*
+// @match        https://piped.privacydev.net/watch?v=*
+// @match        https://piped.smnz.de/watch?v=*
+// @match        https://piped.adminforge.de/watch?v=*
+// @match        https://piped.astartes.nl/watch?v=*
+// @match        https://piped.drgns.space/watch?v=*
+// @match        https://piped.ngn.tf/watch?v=*
+// @match        https://piped.us.projectsegfau.lt/watch?v=*
+// @match        https://piped.ducks.party/watch?v=*
 // ==/UserScript==
 
 (() => {
