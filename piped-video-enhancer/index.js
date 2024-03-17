87e66fdfa4fd7566a36fa6b93d45b2c6ddd1a4d0
GitHub Action <action@github.com>
2024-03-17 02:24:33 +0000

    Update piped-video-enhancer domains

--- piped-video-enhancer/index.js
+++ piped-video-enhancer/index.js
@@ -10,7 +10,22 @@
 // @run-at       document-idle
 // @icon         https://cdn.statically.io/gh/TeamPiped/Piped/3a78b19a/public/favicon.ico
 // ==/UserScript==
-
+// @match        https://pipedapi.kavin.rocks/watch?v=*
+// @match        https://pipedapi.syncpundit.io/watch?v=*
+// @match        https://piped-api.lunar.icu/watch?v=*
+// @match        https://pipedapi.r4fo.com/watch?v=*
+// @match        https://pipedapi.darkness.services/watch?v=*
+// @match        https://pipedapi-libre.kavin.rocks/watch?v=*
+// @match        https://api.piped.projectsegfau.lt/watch?v=*
+// @match        https://pipedapi.us.projectsegfau.lt/watch?v=*
+// @match        https://api.piped.privacydev.net/watch?v=*
+// @match        https://pipedapi.smnz.de/watch?v=*
+// @match        https://pipedapi.adminforge.de/watch?v=*
+// @match        https://pipedapi.astartes.nl/watch?v=*
+// @match        https://pipedapi.drgns.space/watch?v=*
+// @match        https://pipedapi.ducks.party/watch?v=*
+// @match        https://pipedapi.ngn.tf/watch?v=*
+// @match        https://pipedapi.coldforge.xyz/watch?v=*
 (() => {
     'use strict';
 
