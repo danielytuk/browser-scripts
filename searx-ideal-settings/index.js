b374e634ad18da783b29a351c437c82edb27489d
Daniel Back <daniel@dytuk.media>
2024-03-17 09:57:16 +0000

    Create index.js

---
+++ searx-ideal-settings/index.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Ideal Search Settings
+// @description  Automatically sets ideal search parameters for enhanced search experience
+// @version      1.0
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(() => {
+    'use strict';
+
+    // Add desired query parameters to the URL before visiting the page
+    const urlString = new URL(window.location.href);
+    urlString.searchParams.set("category_general", "1");
+    urlString.searchParams.set("language", "auto");
+    urlString.searchParams.set("time_range", "");
+    urlString.searchParams.set("safesearch", "0");
+    urlString.searchParams.set("theme", "simple");
+    window.location.href = urlString.href;
+})();
