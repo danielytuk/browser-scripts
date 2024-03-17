d98990ddb0bef37754ca822149c9f8de72ea8876
GitHub Action <action@github.com>
2024-03-17 18:36:13 +0000

    Update piped-video-enhancer domains

--- piped-video-enhancer/index.js
+++ piped-video-enhancer/index.js
@@ -4,24 +4,18 @@
 // @icon         https://cdn.statically.io/gh/TeamPiped/Piped/3a78b19a/public/favicon.ico
 // @grant        none
 // @author       danielytuk
-// @version      1.17
+// @version      1.18
 // @run-at       document-idle
-// @name         Piped Video Enhancer
-// @name         Piped Video Enhancer
-// @match        https://piped-instances.kavin.rocks/watch?v=*
 
 (() => {
     'use strict';
-
     let handleScroll = null;
-
     const applyCinemaMode = (navbar, videoPlayer) => {
         const toggleNavbar = () => navbar.style.display = window.scrollY ? 'block' : 'none';
         Object.assign(videoPlayer.style, { width: '100%', height: 'calc(100vh - 50px)', maxHeight: '131vh' });
         toggleNavbar();
         handleScroll = toggleNavbar;
     };
-
     const select1080pResolution = () => {
         const resolutionsMenu = document.querySelector("#app > div > div.w-full > div:nth-child(1) > div:nth-child(1) > div > div.shaka-controls-container > div.shaka-no-propagation.shaka-show-controls-on-mouse-over.shaka-settings-menu.shaka-resolutions");
         const resolutionButtons = document.querySelectorAll('.explicit-resolution');
@@ -33,31 +27,22 @@
                 }
             }
         }
-    };
-
     const checkForElements = () => {
         const navbar = document.querySelector('nav'), videoPlayer = document.querySelector('.player-container');
         if (navbar && videoPlayer) {
             applyCinemaMode(navbar, videoPlayer);
             select1080pResolution();
-        }
-    };
-
     const observer = new MutationObserver(checkForElements);
     observer.observe(document.body, { childList: true, subtree: true });
-
     const scrollHandler = () => handleScroll && handleScroll();
-
     window.addEventListener('scroll', scrollHandler);
-
     window.addEventListener('beforeunload', (event) => {
         if (handleScroll) {
             handleScroll = null;
             window.removeEventListener('scroll', scrollHandler);
             observer.disconnect();
             event.preventDefault();
-        }
     });
-
     window.addEventListener('DOMContentLoaded', checkForElements);
 })();
+checkForElements);
