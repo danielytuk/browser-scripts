d14090500807726f20d59481b9041674b51c99af
GitHub Action <action@github.com>
2024-03-21 02:16:35 +0000

    Update piped-video-enhancer domains

--- piped-video-enhancer/index.js
+++ piped-video-enhancer/index.js
@@ -5,7 +5,7 @@
 // @require      https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
 // @grant        none
 // @author       danielytuk
-// @version      1.28
+// @version      1.29
 // @run-at       document-idle
 // @match        https://piped.lunar.icu/watch?v=*
 // @match        https://piped.projectsegfau.lt/watch?v=*
