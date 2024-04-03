d0abdcec84eae2756f59eed87a410dae43f36caf
GitHub Action <action@github.com>
2024-04-03 01:42:28 +0000

    Update piped-video-enhancer domains

--- piped-video-enhancer/index.js
+++ piped-video-enhancer/index.js
@@ -4,41 +4,39 @@
 // @icon         https://cdn.statically.io/gh/TeamPiped/Piped/3a78b19a/public/favicon.ico
 // @grant        none
 // @author       danielytuk
-// @version      1.53
+// @version      1.54
 // @run-at       document-idle
 // @downloadURL  https://cdn.jsdelivr.net/gh/danielytuk/browser-scripts/piped-video-enhancer/index.js
 // @match        https://piped.lunar.icu/watch?v=*
+// @match        https://piped.darkness.services/watch?v=*
 // @match        https://piped.projectsegfau.lt/watch?v=*
 // @match        https://piped.in.projectsegfau.lt/watch?v=*
+// @match        https://piped.us.projectsegfau.lt/watch?v=*
 // @match        https://piped.privacydev.net/watch?v=*
 // @match        https://piped.smnz.de/watch?v=*
+// @match        https://piped.adminforge.de/watch?v=*
 // @match        https://piped.astartes.nl/watch?v=*
 // @match        https://piped.drgns.space/watch?v=*
 // @match        https://pi.ggtyler.dev/watch?v=*
 // @match        https://piped.12a.app/watch?v=*
-// @match        https://piped.ngn.tf/watch?v=*
+// @match        https://yt.cdsp.cz/watch?v=*
 // @match        https://piped.ducks.party/watch?v=*
 // ==/UserScript==
 
 (async () => {
     "use strict";
-
     const applyCinemaMode = () => {
         const videoElement = document.querySelector(".player-container");
         const navbar = document.querySelector("nav");
-
         if (videoElement) {
             videoElement.style.width = "100%";
             videoElement.style.height = "calc(100vh - 50px)";
             videoElement.style.maxHeight = "131vh";
         }
-
         if (navbar) {
             navbar.style.display = window.scrollY ? "block" : "none";
             window.addEventListener("scroll", () => navbar.style.display = window.scrollY ? "block" : "none");
-        }
     };
-
     const set1080pResolution = () => {
         const resolutions = document.querySelectorAll(".shaka-resolution");
         resolutions.forEach(resolution => {
@@ -46,23 +44,15 @@
                 resolution.click();
             }
         });
-    };
-
     const checkForElements = () => {
-        const navbar = document.querySelector("nav");
         const video = document.querySelector(".player-container");
-
         if (navbar && video) {
             applyCinemaMode();
             set1080pResolution();
         } else {
             requestAnimationFrame(checkForElements);
-        }
-    };
-
     const debounceScroll = () => {
         let scrollHandler;
-
         const scrollListener = () => {
             if (!scrollHandler) {
                 scrollHandler = () => {
@@ -72,19 +62,13 @@
                     });
                 };
                 requestAnimationFrame(scrollHandler);
-            }
         };
-
         window.addEventListener("scroll", scrollListener);
-    };
-
     const disconnectBeforeUnload = () => {
         window.addEventListener("beforeunload", () => {
             window.removeEventListener("scroll", debounceScroll);
             document.defaultView.disconnect();
         }, 0);
-    };
-
     window.addEventListener("DOMContentLoaded", () => {
         checkForElements();
         debounceScroll();
