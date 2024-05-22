5dd4086b738dd456aaa182ca15ece097b17fc0d8
GitHub Action <action@github.com>
2024-05-22 01:51:48 +0000

    Update piped-video-enhancer domains

--- piped-video-enhancer/index.js
+++ piped-video-enhancer/index.js
@@ -4,22 +4,25 @@
 // @icon         https://cdn.statically.io/gh/TeamPiped/Piped/3a78b19a/public/favicon.ico
 // @grant        none
 // @author       danielytuk
-// @version      1.94
+// @version      1.95
 // @run-at       document-idle
 // @downloadURL  https://cdn.jsdelivr.net/gh/danielytuk/browser-scripts/piped-video-enhancer/index.js
-// @match        https://piped.lunar.icu/watch?v=*
 // @match        https://piped.projectsegfau.lt/watch?v=*
 // @match        https://piped.in.projectsegfau.lt/watch?v=*
+// @match        https://piped.us.projectsegfau.lt/watch?v=*
 // @match        https://piped.privacydev.net/watch?v=*
 // @match        https://piped.smnz.de/watch?v=*
 // @match        https://piped.adminforge.de/watch?v=*
 // @match        https://piped.astartes.nl/watch?v=*
 // @match        https://piped.drgns.space/watch?v=*
-// @match        https://pi.ggtyler.dev/watch?v=*
-// @match        https://piped.ducks.party/watch?v=*
+// @match        https://piped.ngn.tf/watch?v=*
 // @match        https://yt.cdsp.cz/watch?v=*
+// @match        https://piped.reallyaweso.me/watch?v=*
 // @match        https://piped.phoenixthrush.com/watch?v=*
 // @match        https://piped.private.coffee/watch?v=*
+// @match        https://piped.dedyn.io/watch?v=*
+// @match        https://piped.nosebs.ru/watch?v=*
+// @match        https://piped.ducks.party/watch?v=*
 // ==/UserScript==
 
 (async () => {
