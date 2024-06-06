02aba06e75098d494937e89b7be442f6f143fb1c
GitHub Action <action@github.com>
2024-06-06 01:53:44 +0000

    Update piped-video-enhancer domains

--- piped-video-enhancer/index.js
+++ piped-video-enhancer/index.js
@@ -4,7 +4,7 @@
 // @icon         https://cdn.statically.io/gh/TeamPiped/Piped/3a78b19a/public/favicon.ico
 // @grant        none
 // @author       danielytuk
-// @version      1.108
+// @version      1.109
 // @run-at       document-idle
 // @downloadURL  https://cdn.jsdelivr.net/gh/danielytuk/browser-scripts/piped-video-enhancer/index.js
 // @match        https://piped.lunar.icu/watch?v=*
@@ -16,15 +16,17 @@
 // @match        https://piped.adminforge.de/watch?v=*
 // @match        https://piped.frontendfriendly.xyz/watch?v=*
 // @match        https://piped.yt/watch?v=*
+// @match        https://piped.astartes.nl/watch?v=*
 // @match        https://piped.drgns.space/watch?v=*
 // @match        https://pi.ggtyler.dev/watch?v=*
+// @match        https://piped.ducks.party/watch?v=*
 // @match        https://piped.ngn.tf/watch?v=*
 // @match        https://yt.cdsp.cz/watch?v=*
 // @match        https://piped.reallyaweso.me/watch?v=*
 // @match        https://piped.phoenixthrush.com/watch?v=*
 // @match        https://piped.private.coffee/watch?v=*
+// @match        https://piped.dedyn.io/watch?v=*
 // @match        https://piped.nosebs.ru/watch?v=*
-// @match        https://piped.ducks.party/watch?v=*
 // ==/UserScript==
 
 (async () => {
