a47cf6b6b1acfa6118ea270054c4d9670f69ac31
GitHub Action <action@github.com>
2024-08-21 02:00:21 +0000

    Update piped-video-enhancer domains

--- piped-video-enhancer/index.js
+++ piped-video-enhancer/index.js
@@ -4,18 +4,17 @@
 // @icon         https://cdn.statically.io/gh/TeamPiped/Piped/3a78b19a/public/favicon.ico
 // @grant        none
 // @author       danielytuk
-// @version      1.167
+// @version      1.168
 // @run-at       document-idle
 // @downloadURL  https://cdn.jsdelivr.net/gh/danielytuk/browser-scripts/piped-video-enhancer/index.js
 // @match        https://piped.projectsegfau.lt/watch?v=*
 // @match        https://piped.in.projectsegfau.lt/watch?v=*
+// @match        https://piped.smnz.de/watch?v=*
 // @match        https://piped.adminforge.de/watch?v=*
 // @match        https://piped.yt/watch?v=*
 // @match        https://piped.astartes.nl/watch?v=*
 // @match        https://piped.drgns.space/watch?v=*
-// @match        https://piped.cdsp.cz/watch?v=*
 // @match        https://piped.phoenixthrush.com/watch?v=*
-// @match        https://piped.private.coffee/watch?v=*
 // @match        https://piped.privacydev.net/watch?v=*
 // @match        https://piped.ducks.party/watch?v=*
 // ==/UserScript==
